5eb8ee80757cd3e0c67df747c0bbb3aa9d76c399
julialonghi <116280754+julialonghi@users.noreply.github.com>
2024-05-27 08:45:20 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+num1= float(input("Digite o primeiro número: "))
+num2= float(input("Digite o segundo número: "))
+
+soma= num1+num2
+diferenca =  num1-num2
+produto = num1*num2
+divisao = num1/num2
+
+print("A soma dos números é: ", soma)
+print("A diferença dos números é: ", diferenca")
+print("O produto dos números é: ", produto)
+print("A divisão dos números é: ", divisao)
